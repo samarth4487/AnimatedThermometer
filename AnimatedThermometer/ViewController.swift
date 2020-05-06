10a008cd3d43f5759cbe491fd2f30300fc10e621
samarth4487 <samarthpaboowal@yahoo.co.in>
2020-05-06 23:46:44 +0530

    Initial Commit

---
+++ AnimatedThermometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatedThermometer
+//
+//  Created by Samarth Paboowal on 06/05/20.
+//  Copyright © 2020 Samarth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
